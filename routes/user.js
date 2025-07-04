2e3988b457f93b64bc33d3f70ab0854244d82d86
inazrabuu <inazrabuu@gmail.com>
2025-07-04 20:37:20 +0700

    remove debugger

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,6 @@ const express = require('express'),
 
 route.get('/:id', async (req, res) => {
   const userId = req.params.id
-  console.log('here')
 
   if (userId !== '123') {
     throw new NotFoundError('User Not Found!')
