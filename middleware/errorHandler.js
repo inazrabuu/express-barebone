9e15fb64a642879495f6225083f87ab78abdae36
inazrabuu <inazrabuu@gmail.com>
2025-07-06 20:16:09 +0700

    adding response wrapper

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -11,11 +11,8 @@ module.exports = (err, req, res, next) => {
   const statusCode = err.statusCode || 500,
         message = err.message || 'Internal Server Error'
 
-  res.status(statusCode).json({
-    success: false,
-    error: {
-      name: err.name,
-      message: message
-    }
-  })
+  res.fail({
+    name: err.name,
+    message: message
+  }, statusCode)
 }
