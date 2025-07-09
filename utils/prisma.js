992859edfd2fd7bfa8f5644a4441bddea0c96d3c
inazrabuu <inazrabuu@gmail.com>
2025-07-09 17:31:48 +0700

    tidy up Prisma

--- utils/prisma.js
+++ utils/prisma.js
@@ -1,4 +1,26 @@
 const { PrismaClient } = require('@prisma/client'),
       prisma = new PrismaClient()
 
+async function initPrisma() {
+  try {
+    const dbUrl = process.env.DATABASE_URL
+    let dbType = 'Unknown'
+
+    await prisma.$connect()
+
+    if (dbUrl.startsWith('postgres')) dbType = 'Postgresql'
+    else if (dbUrl.startsWith('mysql')) dbType = 'MySql'
+    else if (dbUrl.startsWith('sqlite')) dbType = 'SQLite'
+    else if (dbUrl.startsWith('sqlserver')) dbType = 'SQL Server'
+    else if (dbUrl.startsWith('mongo')) dbType = 'MongoDB'
+
+    console.log(`✅ ${dbType} connected via Prisma`)
+  } catch (err) {
+    console.error('❌ Failed to connect to database:', err)
+    process.exit(1)
+  }
+}
+
+initPrisma()
+
 module.exports = prisma
